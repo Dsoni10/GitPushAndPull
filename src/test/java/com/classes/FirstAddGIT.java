a0ec5c5d4a16808bd26f4969cd08fda2eee5aad5
John Doe <deshmukh.soni1993@gmail.com>
2023-04-24 19:11:05 +0530

    second commit for ABC.TXT FILE

---
+++ src/test/java/com/classes/FirstAddGIT.java
@@ -0,0 +1,11 @@
+package com.classes;
+
+import java.net.StandardSocketOptions;
+
+public class FirstAddGIT {
+
+    public void main(String args[]){
+        System.out.println("FirstScript");
+    }
+
+}
